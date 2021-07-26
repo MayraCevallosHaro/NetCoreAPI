191f5fedbac7ec7ca3e1e62c521ba690c62801d0
MayraCevallosHaro <mayra.cevallos2016@uteq.edu.ec>
2021-07-26 17:41:33 -0500

    Controller

---
+++ NetCoreAPI/Pages/CarController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreAPI.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreAPI.Pages
+{
+    [Route("api/[controler]")]
+    [ApiController]
+    public class CarController : Controller
+    {
+
+        private readonly ICarRepository _carRepository;
+
+        public CarController(ICarRepository carRepository)
+        {
+            _carRepository = carRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCars()
+        {
+            return Ok(await _carRepository.GetAllCars());
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCarDetails(int id)
+        {
+            return Ok(await _carRepository.GetCarDetails(id));
+        }
+
+    }
+}
