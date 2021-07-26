dcdb54c0e2871878a27cfb5b7c5d62faef6c2a5d
MayraCevallosHaro <mayra.cevallos2016@uteq.edu.ec>
2021-07-26 14:49:52 -0500

    Implementar Interfase

---
+++ NetCoreAPI.Data/Repositories/CarRepository.cs
@@ -0,0 +1,37 @@
+﻿using NetCoreAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoreAPI.Data.Repositories
+{
+    class CarRepository : ICarRepository
+    {
+        public Task<Car> DeleteCar(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Car>> GetAllCars()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Car> GetCarDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Car> InsertCar(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Car> UpdateCar(Car car)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
