4ef6992348181832a332262a2dd70a69d53f9a14
MayraCevallosHaro <mayra.cevallos2016@uteq.edu.ec>
2021-07-26 14:45:28 -0500

    Médotos Asíncronos

---
+++ NetCoreAPI.Data/Repositories/ICarRepository.cs
@@ -0,0 +1,18 @@
+﻿using NetCoreAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoreAPI.Data.Repositories
+{
+    public interface ICarRepository
+    {
+        Task<IEnumerable<Car>> GetAllCars();
+        Task<Car> GetCarDetails(int id);
+        Task<Car> InsertCar(Car car);
+        Task<Car> UpdateCar(Car car);
+        Task<Car> DeleteCar(Car car);
+    }
+}
